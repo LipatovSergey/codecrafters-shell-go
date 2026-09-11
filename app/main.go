071e66984181115002f367391625c5c06c72d6d8
LipatovSergey <lipatovsergey01@gmail.com>
2026-09-12 02:00:26 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,11 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+	path := os.Getenv("PATH")
+	dirs := filepath.SplitList(path)
 	builtInCommands := map[string]struct{}{
 		"echo": {},
 		"exit": {},
@@ -29,9 +32,21 @@ func main() {
 			command := input[5:]
 			if _, ok := builtInCommands[command]; ok {
 				fmt.Println(command + " is a shell builtin")
-			} else {
-				fmt.Println(command + ": not found")
+				continue
 			}
+			for _, dir := range dirs {
+				fullpath := filepath.Join(dir, command)
+				info, err := os.Stat(fullpath)
+				if err != nil {
+					continue
+				}
+				if info.Mode().Perm()&0o111 == 0 {
+					continue
+				}
+				fmt.Println(command, "is", fullpath)
+				break
+			}
+			fmt.Println(command + ": not found")
 			continue
 		}
 		fmt.Println(input + ": command not found")
