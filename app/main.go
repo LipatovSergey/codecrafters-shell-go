0aa0926aa025e02636bb0b31d84871d7a62131ec
LipatovSergey <lipatovsergey01@gmail.com>
2026-09-12 02:54:22 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,14 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
+func isExecutable(command string) (string, bool) {
 	path := os.Getenv("PATH")
 	dirs := filepath.SplitList(path)
+	for _, dir := range dirs {
+		fullpath := filepath.Join(dir, command)
+		info, err := os.Stat(fullpath)
+		if err == nil && info.Mode().Perm()&0o111 != 0 {
+			return fullpath, true
+		}
+	}
+	return "", false
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
 	builtInCommands := map[string]struct{}{
 		"echo": {},
 		"exit": {},
@@ -34,20 +46,24 @@ func main() {
 				fmt.Println(command + " is a shell builtin")
 				continue
 			}
-			found := false
-			for _, dir := range dirs {
-				fullpath := filepath.Join(dir, command)
-				info, err := os.Stat(fullpath)
-				if err == nil && info.Mode().Perm()&0o111 != 0 {
-					found = true
-					fmt.Println(command, "is", fullpath)
-					break
-				}
+			fullpath, found := isExecutable(command)
+			if found {
+				fmt.Println(command, "is", fullpath)
 				continue
 			}
-			if !found {
-				fmt.Println(command + ": not found")
-			}
+			fmt.Println(command + ": not found")
+			continue
+		}
+		inputFields := strings.Fields(input)
+		programName := inputFields[0]
+		_, found := isExecutable(programName)
+		if found {
+			programArgs := inputFields[1:]
+			cmd := exec.Command(programName, programArgs...)
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Run()
 			continue
 		}
 		fmt.Println(input + ": command not found")
